3cee74df7eb7d6949d511b093bd48b28608e6081
spemer <ghsspower@gmail.com>
2019-07-02 09:49:20 +0900

    ADD vue-lodash, firebase hosting

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import "./registerServiceWorker"
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+import VueLodash from 'vue-lodash'
+Vue.use(VueLodash)
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#app")
